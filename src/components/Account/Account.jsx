1a74153759ccd00f07b1785961ea9f21b051ea61
Flein890 <emifran2812@outlook.com>
2024-08-28 15:34:45 -0300

    c

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -6,12 +6,12 @@ import { useSelector } from "react-redux";
 function Account(){
     
     //traemos el username del state
-    const userName = useSelector((state) => state.user.currentUser.name)
-    console.log(userName)
+    // const userName = useSelector((state) => state.user.currentUser.name)
+    // console.log(userName)
 
     return(
     <AccountUserStyled>
-        {userName && <><span>{userName}</span></>} 
+
         <i className="fa-solid fa-user"></i>
     </AccountUserStyled>
     )
